d423323cdd60b5b2c55647a2f22bbd7daa0ae9e7
Seph <seph@prnsofts-iMac.local>
2013-12-17 10:27:23 +0900

    Create base iOS XCode Project

---
+++ iOS/SmartDeviceApp/Classes/Controllers/AppLauncher/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SmartDeviceApp
+//
+//  Created by Seph on 12/17/13.
+//  Copyright (c) 2013 aLink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/SmartDeviceApp/Classes/Controllers/ViewControllers/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SmartDeviceApp
+//
+//  Created by Seph on 12/17/13.
+//  Copyright (c) 2013 aLink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
