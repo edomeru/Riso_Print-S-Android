b38a8aaaa1d7296f2046607b8291c262e86e5bca
Seph <seph@prnsofts-iMac.local>
2013-12-17 10:58:20 +0900

    Add base iOS unit test

---
+++ iOS/SmartDeviceAppTests/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SmartDeviceAppTests
+//
+//  Created by Seph on 12/17/13.
+//  Copyright (c) 2013 aLink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+    }
+}
