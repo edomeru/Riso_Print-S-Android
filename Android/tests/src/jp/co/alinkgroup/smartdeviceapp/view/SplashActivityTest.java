e8c241eb646989e8090c921c4d640c60add4c987
paulus <paulus@alink-group.co.jp>
2014-01-28 14:41:44 +0900

    #3534 initial instrumentation test

--- Android/tests/src/jp/co/alinkgroup/smartdeviceapp/view/SplashActivityTest.java
+++ Android/tests/src/jp/co/alinkgroup/smartdeviceapp/view/SplashActivityTest.java
@@ -1,9 +1,13 @@
 
 package jp.co.alinkgroup.smartdeviceapp.view;
 
+import android.app.Instrumentation;
+import android.content.pm.ActivityInfo;
 import android.test.ActivityInstrumentationTestCase2;
 
 public class SplashActivityTest extends ActivityInstrumentationTestCase2<SplashActivity> {
+    private Instrumentation mInstrumentation;
+    private SplashActivity mActivity; // MyActivity is the class name of the app under test
     
     public SplashActivityTest() {
         super(SplashActivity.class);
@@ -15,14 +19,59 @@ public class SplashActivityTest extends ActivityInstrumentationTestCase2<SplashA
 
     protected void setUp() throws Exception {
         super.setUp();
+        
+        mInstrumentation = getInstrumentation();
     }
     
     protected void tearDown() throws Exception {
         super.tearDown();
+        
     }
     
-    public void testIfTestWorks() {
+    
+    public void testLifecyle_PauseResume() {
+        mActivity = getActivity();
         
-        assertTrue(true);
+        try {
+            mActivity.onPause();
+            mActivity.onResume();
+        } catch (Exception e){
+            fail(e.getMessage());
+        }
+        
+        mInstrumentation.waitForIdleSync();
+        
+        mActivity.finish();
     }
+    
+    public void testLifecycle_Rotation() {
+        mActivity = getActivity();
+        
+        mActivity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        mInstrumentation.waitForIdleSync();
+        
+        mActivity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        mInstrumentation.waitForIdleSync();
+        
+        mActivity.finish();
+    }
+    
+    /*
+    public void testLifecycle_MainActivityShouldDisplay() {
+        mActivity = getActivity();
+        
+        mInstrumentation.waitForIdleSync();
+
+        // assume AsyncTask will be finished in 6 seconds.
+        try {
+          Thread.sleep(AppConstants.APP_SPLASH_DURATION);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        
+        mInstrumentation.waitForIdleSync();
+        
+        assertTrue(mActivity.isFinishing());
+    }
+    */
 }
