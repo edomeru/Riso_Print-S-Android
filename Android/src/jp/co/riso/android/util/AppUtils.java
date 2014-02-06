1932e3c766d4060b89c97233ab114fe8a2c69449
paulus <paulus@alink-group.co.jp>
2014-02-06 17:16:58 +0900

    #3534 renamed package

---
+++ Android/src/jp/co/riso/android/util/AppUtils.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2014 All rights reserved.
+ *
+ * AppUtils.java
+ * SmartDeviceApp
+ * Created by: a-LINK Group
+ */
+package jp.co.riso.android.util;
+
+import java.io.File;
+import java.util.Locale;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.util.AndroidRuntimeException;
+
+public final class AppUtils {
+    
+    /**
+     * Creates an activity intent launcher
+     * 
+     * @param context
+     *            Application Context
+     * @param cls
+     *            Activity class
+     * @return Intent generated
+     */
+    public static Intent createActivityIntent(Context context, Class<?> cls) {
+        // Should not be created if context or class is null
+        if (context == null || cls == null) {
+            return null;
+        }
+        
+        Intent intent = new Intent();
+        intent.setClass(context, cls);
+        return intent;
+    }
+    
+    /**
+     * Starts an Activity
+     * 
+     * @param context
+     *            Application Context
+     * @param cls
+     *            Activity class
+     */
+    public static void startActivityIntent(Context context, Class<?> cls) throws ActivityNotFoundException, NullPointerException, AndroidRuntimeException {
+        Intent intent = createActivityIntent(context, cls);
+        
+        if (intent == null) {
+            throw new NullPointerException("Cannot create intent");
+        } else {
+            try {
+                context.startActivity(intent);
+            } catch (ActivityNotFoundException e) {
+                throw e;
+            } catch (AndroidRuntimeException e) {
+                throw e;
+            }
+        }
+    }
+    
+    /**
+     * Gets the 2 character locale code based on the current Locale. (e.g., en, ja, etc)
+     * 
+     * @return Locale Code String
+     */
+    public static String getLocaleCode() {
+        Locale defaultLocale = Locale.getDefault();
+        String localeCode = defaultLocale.toString().substring(0, 2).toLowerCase(defaultLocale);
+        
+        return localeCode;
+    }
+    
+    /**
+     * Gets the Application package name
+     * 
+     * @param context
+     *            Application Context
+     * @return Package name of the application
+     */
+    public static String getApplicationPackageName(Context context) {
+        if (context == null) {
+            return null;
+        }
+        
+        return context.getPackageName();
+    }
+    
+    /**
+     * Gets the Application install date using the package manager
+     * 
+     * @param context
+     *            Application Context
+     * 
+     * @return Time in millis of the the last install date
+     */
+    public static long getApplicationLastInstallDate(Context context, String packageName) throws NameNotFoundException {
+        if (context == null) {
+            return 0;
+        }
+        
+        PackageManager pm = context.getPackageManager();
+        ApplicationInfo appInfo = null;
+        
+        try {
+            appInfo = pm.getApplicationInfo(packageName, 0);
+        } catch (NameNotFoundException e) {
+            throw e;
+        }
+        
+        String appFilePath = appInfo.sourceDir;
+        File appFile = new File(appFilePath);
+        
+        return appFile.lastModified();
+    }
+}
---
+++ Android/src/jp/co/riso/smartdeviceapp/view/MainActivity.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2014 All rights reserved.
+ *
+ * MainActivity.java
+ * SmartDeviceApp
+ * Created by: a-LINK Group
+ */
+package jp.co.riso.smartdeviceapp.view;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import jp.co.riso.smartdeviceapp.R;
+import jp.co.riso.smartdeviceapp.view.base.BaseActivity;
+import jp.co.riso.smartdeviceapp.view.fragment.SideBarFragment;
+import jp.co.riso.smartdeviceapp.view.fragment.HomePreviewFragment;
+
+public class MainActivity extends BaseActivity {
+    
+    public static final String KEY_TRANSLATION = "translate";
+    
+    private DrawerLayout mDrawerLayout;
+    private ViewGroup mMainLayout;
+    private ViewGroup mLeftLayout;
+    private ViewGroup mRightLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    
+    @Override
+    protected void onCreateContent(Bundle savedInstanceState) {
+        
+        setContentView(R.layout.activity_main);
+        
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mMainLayout = (ViewGroup) findViewById(R.id.mainLayout);
+        mLeftLayout = (ViewGroup) findViewById(R.id.leftLayout);
+        mRightLayout = (ViewGroup) findViewById(R.id.rightLayout);
+        
+        mDrawerLayout.setScrimColor(Color.TRANSPARENT);
+        
+        mDrawerToggle = new SDAActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, R.string.default_content_description,
+                R.string.default_content_description);
+        
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        
+        if (getActionBar() != null) {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+            getActionBar().setHomeButtonEnabled(true);
+        }
+        
+        // Begin Fragments
+        if (savedInstanceState == null) {
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            
+            ft.add(R.id.mainLayout, new HomePreviewFragment());
+            ft.add(R.id.leftLayout, new SideBarFragment());
+            
+            ft.commit();
+        } else {
+            mMainLayout.setTranslationX(savedInstanceState.getFloat(KEY_TRANSLATION, 0.0f));
+        }
+    }
+    
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+    
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        
+        outState.putFloat(KEY_TRANSLATION, mMainLayout.getTranslationX());
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+        
+        return super.onOptionsItemSelected(item);
+    }
+    
+    // ================================================================================
+    // Public Functions
+    // ================================================================================
+    
+    public void openDrawer(int gravity) {
+        mDrawerLayout.openDrawer(gravity);
+    }
+    
+    public void closeDrawers() {
+        mDrawerLayout.closeDrawers();
+    }
+    
+    // ================================================================================
+    // Internal Classes
+    // ================================================================================
+    
+    private class SDAActionBarDrawerToggle extends ActionBarDrawerToggle {
+        
+        public SDAActionBarDrawerToggle(Activity activity, DrawerLayout drawerLayout, int drawerImageRes, int openDrawerContentDescRes,
+                int closeDrawerContentDescRes) {
+            super(activity, drawerLayout, drawerImageRes, openDrawerContentDescRes, closeDrawerContentDescRes);
+        }
+        
+        @Override
+        public void onDrawerSlide(View drawerView, float slideOffset) {
+            float moveFactor = (mLeftLayout.getWidth() * slideOffset);
+            if (drawerView.getId() == mRightLayout.getId()) {
+                moveFactor *= -1;
+            }
+            
+            mMainLayout.setTranslationX(moveFactor);
+        }
+        
+        @Override
+        public void onDrawerStateChanged(int newState) {
+            
+            super.onDrawerStateChanged(newState);
+            
+            if (newState == DrawerLayout.STATE_IDLE) {
+                if (mDrawerLayout.isDrawerOpen(Gravity.START)) {
+                    mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED, Gravity.END);
+                } else if (mDrawerLayout.isDrawerOpen(Gravity.END)) {
+                    mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED, Gravity.START);
+                } else {
+                    mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+                }
+            }
+            
+        }
+        
+        /**
+         * Called when a drawer has settled in a completely closed state.
+         */
+        public void onDrawerClosed(View view) {
+            super.onDrawerClosed(view);
+            invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+        }
+        
+        /**
+         * Called when a drawer has settled in a completely opened state.
+         */
+        public void onDrawerOpened(View drawerView) {
+            super.onDrawerOpened(drawerView);
+            invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+        }
+    }
+}
