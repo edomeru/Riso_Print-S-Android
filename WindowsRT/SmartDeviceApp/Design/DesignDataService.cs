fa40ca9a2ec348d01d71e1723551bc03837c15dc
karen <karen@alink-group.co.jp>
2014-02-25 19:51:12 +0900

    Windows RT base repository

---
+++ WindowsRT/SmartDeviceApp/Design/DesignDataService.cs
@@ -0,0 +1,28 @@
+﻿//
+//  DesignDataService.cs
+//  SmartDeviceApp
+//
+//  Created by a-LINK Group on 2014/02/25.
+//  Copyright 2014 RISO KAGAKU CORPORATION. All Rights Reserved.
+//
+//  Revision History :
+//  Date            Author/ID           Ver.
+//  ----------------------------------------------------------------------
+//
+
+using System.Threading.Tasks;
+using SmartDeviceApp.Models;
+
+namespace SmartDeviceApp.Design
+{
+    public class DesignDataService : IDataService
+    {
+        public Task<DataItem> GetData()
+        {
+            // Use this to create design time data
+
+            var item = new DataItem("Welcome to MVVM Light [design]");
+            return Task.FromResult(item);
+        }
+    }
+}
---
+++ WindowsRT/SmartDeviceApp/Models/IDataService.cs
@@ -0,0 +1,21 @@
+﻿//
+//  IDataService.cs 
+//  SmartDeviceApp
+//
+//  Created by a-LINK Group on 2014/02/25.
+//  Copyright 2014 RISO KAGAKU CORPORATION. All Rights Reserved.
+//
+//  Revision History :
+//  Date            Author/ID           Ver.
+//  ----------------------------------------------------------------------
+//
+
+using System.Threading.Tasks;
+
+namespace SmartDeviceApp.Models
+{
+    public interface IDataService
+    {
+        Task<DataItem> GetData();
+    }
+}
