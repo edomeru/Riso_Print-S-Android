cccaa0547ff347b4975fb11a93bec0a62c3b4173
Paulus <paulus@alink-group.co.jp>
2014-03-11 18:10:07 +0900

    #3584 Added initial implementation of right layout

--- Android/src/jp/co/riso/smartdeviceapp/view/base/BaseActivity.java
+++ Android/src/jp/co/riso/smartdeviceapp/view/base/BaseActivity.java
@@ -8,9 +8,11 @@
 
 package jp.co.riso.smartdeviceapp.view.base;
 
+import jp.co.riso.android.util.AppUtils;
 import jp.co.riso.smartdeviceapp.R;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.util.TypedValue;
 
@@ -53,5 +55,18 @@ public abstract class BaseActivity extends Activity {
             return TypedValue.complexToDimensionPixelSize(tv.data, getResources().getDisplayMetrics());
         }
         return -1;
+    }
+    
+    public int getDrawerWidth() {
+        Point screenSize = AppUtils.getScreenDimensions(this);
+        float drawerWidthPercentage = getResources().getFraction(R.dimen.drawer_width_percentage, 1, 1);
+        float minDrawerWidth = getResources().getDimension(R.dimen.drawer_width_min);
+        float maxDrawerWidth = getResources().getDimension(R.dimen.drawer_width_max);
+        
+        float drawerWidth = screenSize.x * drawerWidthPercentage;
+        drawerWidth = Math.max(drawerWidth, minDrawerWidth);
+        drawerWidth = Math.min(drawerWidth, maxDrawerWidth);
+        
+        return (int) drawerWidth;
     }
 }
