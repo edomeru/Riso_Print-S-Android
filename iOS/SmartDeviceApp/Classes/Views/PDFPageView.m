9a3b3d65f3798978e3bbc9244a017057c02b89d2
Amor Corazon S. Rio <amor.rio@i-bridge.com.ph>
2014-03-20 14:53:27 +0800

    Fix scale to fit implementation. Revised floating point comparisons

--- iOS/SmartDeviceApp/Classes/Views/PDFPageView.m
+++ iOS/SmartDeviceApp/Classes/Views/PDFPageView.m
@@ -57,15 +57,53 @@
         NSLog(@"delegate is nil");
         return;
     }
+    //detect first time to draw view
     if(self.isFirstDraw == YES)
     {
         self.isFirstDraw = NO;
         _initialFrameHeight = self.frame.size.height;
     }
     // Drawing code
-    [self drawPage:rect];
+    [self drawView:rect];
 }
 
+/*Draw the view of the PDFPageView*/
+-(void) drawView: (CGRect) rect
+{
+    //Get the settings needed in the drawing of view
+    //Number of pages per sheet
+    NSUInteger numPagesPerSheet = [self.delegate getNumPages];
+    //Color mode
+    BOOL isGrayScale = [self.delegate isGrayScale];
+    
+    //compute the width and height to be occupied by 1 page in rect based on number of pages per sheet
+    CGFloat pageWidth = [self getPageWidth: rect forNumOfPages:numPagesPerSheet];
+    CGFloat pageHeight = [self getPageHeight: rect forNumOfPages:numPagesPerSheet];
+    
+    //get the current graphics context
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(ctx, [[UIColor whiteColor]CGColor]);
+	CGContextFillRect(ctx, rect);
+    
+    //draw the pages incorporating imposition/pagination
+    CGFloat xOffset = 0;
+    CGFloat yOffset = 0;
+    //drawing is from left to right, top to bottom
+    for(int i = 0; i < numPagesPerSheet; i++)
+    {
+        CGRect pageRect = CGRectMake(xOffset, yOffset, pageWidth, pageHeight);
+        [self drawPDFPage:ctx inRect:pageRect forPageNumber:i inGrayScale:isGrayScale];
+        xOffset += pageWidth;
+        if(fabsf(xOffset - rect.size.width) < pageWidth)//if the difference of the total width and accumulated width is 0 or less than a pageWidth, the row has been filled, go to the next row
+        {
+            xOffset = 0;
+            yOffset += pageHeight;
+        }
+    }
+    //TODO: incorporation of the following settings: Staple,Punch, Impostion order, Scale to Fit Off
+}
+
+/*Draw a PDF page in rect*/
 -(void) drawPDFPage: (CGContextRef) graphicsCtx  inRect: (CGRect) rect forPageNumber:(NSUInteger) pageNumber inGrayScale: (BOOL) isGrayScale;
 {
     CGPDFPageRef pdfPage = [self.delegate getPage:pageNumber];
@@ -75,65 +113,71 @@
   
     //create a bitmap context
     CGColorSpaceRef colorSpace = [self getColorSpace:isGrayScale];
-    CGContextRef ctx = CGBitmapContextCreate(nil, mediaRect.size.width, mediaRect.size.height, 8,0,colorSpace,[self getBitmapInfo:isGrayScale]);
+    CGContextRef bitmapCtx = CGBitmapContextCreate(nil, mediaRect.size.width, mediaRect.size.height, 8,0,colorSpace,[self getBitmapInfo:isGrayScale]);
     
-    //Fill with white
-    CGContextSetFillColorWithColor(ctx, [[UIColor whiteColor]CGColor]);
-	CGContextFillRect(ctx, mediaRect);
+    //Fill with white because pdf white image is transparent
+    CGContextSetFillColorWithColor(bitmapCtx, [[UIColor whiteColor]CGColor]);
+	CGContextFillRect(bitmapCtx, mediaRect);
     
     //Flip the coordinate axis
-    CGContextGetCTM(ctx);
-	CGContextScaleCTM(ctx, 1, -1);
-    CGContextTranslateCTM(ctx, 0, -mediaRect.size.height);
+    CGContextGetCTM(bitmapCtx);
+	CGContextScaleCTM(bitmapCtx, 1, -1);
+    CGContextTranslateCTM(bitmapCtx, 0, -mediaRect.size.height);
     
-    //draw page in context
-    CGContextDrawPDFPage(ctx, pdfPage);
+    //draw page in bitmap context
+    CGContextDrawPDFPage(bitmapCtx, pdfPage);
     
     //transform to image
-    CGImageRef imageRef = CGBitmapContextCreateImage(ctx);
+    CGImageRef imageRef = CGBitmapContextCreateImage(bitmapCtx);
     
     //release the color space and bitmap context
     CGColorSpaceRelease(colorSpace);
-    CGContextRelease(ctx);
+    CGContextRelease(bitmapCtx);
     
-    //draw the image in rect
-    CGContextDrawImage(graphicsCtx, rect, imageRef);
+    //create a rect where image will be drawn
+    CGRect rectToDraw = [self createScaleToFitRectInDisplayRect:rect withPDFRect:mediaRect];
+  
+    //draw the image in current graphics context
+    CGContextDrawImage(graphicsCtx, rectToDraw, imageRef);
     
     //release the image
     CGImageRelease(imageRef);
 }
 
--(void) drawPage: (CGRect) rect
+/*Create rect for scale to fit*/
+-(CGRect) createScaleToFitRectInDisplayRect: (CGRect) displayRect withPDFRect:  (CGRect) pdfRect
 {
-    NSUInteger numPagesPerSheet = [self.delegate getNumPages];
+    CGRect scaleToFitRect = displayRect;
     
-    BOOL isGrayScale = [self.delegate isGrayScale];
-    
-    //compute the width and height to be occupied by 1 page in rect
-    CGFloat pageWidth = [self getPageWidth: rect forNumOfPages:numPagesPerSheet];
-    CGFloat pageHeight = [self getPageHeight: rect forNumOfPages:numPagesPerSheet];
-    
-    //get the graphics context
-    CGContextRef ctx = UIGraphicsGetCurrentContext();
-    CGContextSetFillColorWithColor(ctx, [[UIColor whiteColor]CGColor]);
-	CGContextFillRect(ctx, rect);
-    
-    //draw the pages
-    CGFloat xOffset = 0;
-    CGFloat yOffset = 0;
-    for(int i = 0; i < numPagesPerSheet; i++)
+    /*Scale to fit processing  - pdf page image will retain it's shape and is centered in rect*/
+    //get the ratio of the width and height of display rect and of pdf rect
+    CGFloat pdfWidthHeightRatio = pdfRect.size.width / pdfRect.size.height;
+    CGFloat rectWidthHeightRatio= displayRect.size.width/displayRect.size.height;
+
+    /*if the difference of the ratio of the display rect dimensions with the ratio of pdf dimensions significant,
+     compute new rect that has the same dimension ratio as the pdf and centered in original rect*/
+    if(fabsf(rectWidthHeightRatio - pdfWidthHeightRatio) > 0.09)
     {
-        CGRect pageRect = CGRectMake(xOffset, yOffset, pageWidth, pageHeight);
-        [self drawPDFPage:ctx inRect:pageRect forPageNumber:i inGrayScale:isGrayScale];
-        xOffset += pageWidth;
-        if(xOffset == rect.size.width)
+        //compute adjustment of height and width based on pdf image width-height ratio
+        //retain original width first and adjust height
+        CGFloat targetRectWidth = displayRect.size.width;
+        CGFloat targetRectHeight = targetRectWidth/pdfWidthHeightRatio;
+        //if height is bigger than original display rect height, retain height instead and adjust width
+        if(targetRectHeight > displayRect.size.height)
         {
-            xOffset = 0;
-            yOffset += pageHeight;
+            targetRectHeight = displayRect.size.height;
+            targetRectWidth = targetRectHeight * pdfWidthHeightRatio;
         }
+        //create new rect that is centered in paper
+        CGFloat xPoint = displayRect.origin.x + (displayRect.size.width - targetRectWidth)/2;
+        CGFloat yPoint = displayRect.origin.y + (displayRect.size.height - targetRectHeight)/2;
+        scaleToFitRect  = CGRectMake(xPoint, yPoint, targetRectWidth, targetRectHeight);
     }
+    return scaleToFitRect;
 }
 
+#pragma mark - private helper methods
+/*Determine color space*/
 -(CGColorSpaceRef) getColorSpace: (BOOL) isGrayScale
 {
     if(isGrayScale == YES)
@@ -143,6 +187,7 @@
     return CGColorSpaceCreateDeviceRGB();
 }
 
+/*Determine bitmap info to be used*/
 -(CGBitmapInfo) getBitmapInfo: (BOOL) isGrayScale
 {
     if(isGrayScale == YES)
@@ -152,7 +197,7 @@
     return (CGBitmapInfo)kCGImageAlphaNoneSkipLast;
 }
 
-
+/*Determine width of space to be occupied by a single pdf page in rect based on number of pages per sheet*/
 -(CGFloat) getPageWidth:(CGRect) origRect forNumOfPages: (NSUInteger) numPagesPerSheet
 {
     CGFloat pageWidth = origRect.size.width;
@@ -174,6 +219,7 @@
     return pageWidth;
 }
 
+/*Determine height of space to be occupied by a single pdf page in rect based on number of pages per sheet*/
 -(CGFloat) getPageHeight:(CGRect) origRect forNumOfPages: (NSUInteger) numPagesPerSheet
 {
     CGFloat pageHeight = origRect.size.height;
