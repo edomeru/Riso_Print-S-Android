115e5b4ecad04d3b646c00dc8913ec2bb06b7e12
Seph <seph@alink-group.co.jp>
2014-03-26 21:01:36 +0900

    #3609 -- Add UIColor extension for specifying theme colors

---
+++ iOS/SmartDeviceApp/Classes/Helpers/UIColor+Theme.h
@@ -0,0 +1,23 @@
+//
+//  UIColor+Theme.h
+//  SmartDeviceApp
+//
+//  Created by Seph on 3/26/14.
+//  Copyright (c) 2014 aLink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Theme)
+
++ (UIColor *)blackThemeColor;
++ (UIColor *)whiteThemeColor;
++ (UIColor *)gray1ThemeColor;
++ (UIColor *)gray2ThemeColor;
++ (UIColor *)gray3ThemeColor;
++ (UIColor *)gray4ThemeColor;
++ (UIColor *)purple1ThemeColor;
++ (UIColor *)purple2ThemeColor;
++ (UIColor *)redThemeColor;
+
+@end
