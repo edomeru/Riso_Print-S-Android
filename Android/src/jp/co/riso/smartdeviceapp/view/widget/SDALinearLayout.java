cfc70a842eaf1d46ca84ed4f5648c1f911b7fef2
Paulus <paulus@alink-group.co.jp>
2014-03-31 18:10:03 +0900

    #3615 added base classes for animations

---
+++ Android/src/jp/co/riso/smartdeviceapp/view/widget/SDALinearLayout.java
@@ -0,0 +1,31 @@
+package jp.co.riso.smartdeviceapp.view.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class SDALinearLayout extends LinearLayout {
+    public static final String TAG = "SDALinearLayout";
+
+    public SDALinearLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public SDALinearLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SDALinearLayout(Context context) {
+        super(context);
+    }
+
+    public float getXFraction() {
+        int width = getWidth();
+        return (width == 0) ? 0 : getX() / (float) width;
+    }
+
+    public void setXFraction(float xFraction) {
+        int width = getWidth();
+        setX((width > 0) ? (xFraction * width) : 0);
+    }
+}
