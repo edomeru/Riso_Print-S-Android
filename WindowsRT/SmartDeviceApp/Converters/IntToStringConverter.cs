5621d619c0b8ee919e27c7ef2f2a9f2cf3dfa2ca
karen <karen@alink-group.co.jp>
2014-04-02 17:09:57 +0900

    #3570 Updates for KeyTextBoxControl and KeyToggleSwitchControl

---
+++ WindowsRT/SmartDeviceApp/Converters/IntToStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace SmartDeviceApp.Converters
+{
+    public class IntToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null) return String.Empty;
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null) return 0;
+            var temp = 0;
+            var success = Int32.TryParse(value.ToString(), out temp);
+            if (success) return temp;
+            return 0;
+        }
+    }
+}
