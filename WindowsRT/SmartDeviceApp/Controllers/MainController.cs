df1432fb2e70bf40cd9210274ee3a3109bd04c73
lester <lester.valencia@i-bridge.com.ph>
2014-04-24 11:42:34 +0800

    Settings backend

--- WindowsRT/SmartDeviceApp/Controllers/MainController.cs
+++ WindowsRT/SmartDeviceApp/Controllers/MainController.cs
@@ -67,6 +67,8 @@ namespace SmartDeviceApp.Controllers
         /// <returns>task</returns>
         private static async Task InitializeControllers()
         {
+            // TODO: Verify timing of each initialization
+            SettingController.Instance.Initialize();
             await JobController.Instance.Initialize();
         }
 
