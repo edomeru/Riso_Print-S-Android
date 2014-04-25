15c72cb1b875ede88e8b08934d5e1c20b556c9ea
lester <lester.valencia@i-bridge.com.ph>
2014-04-25 18:25:24 +0800

    Set duplex to off when booklet is off.

--- WindowsRT/SmartDeviceApp/Controllers/PrintPreviewController.cs
+++ WindowsRT/SmartDeviceApp/Controllers/PrintPreviewController.cs
@@ -1429,6 +1429,8 @@ namespace SmartDeviceApp.Controllers
                 if (duplexPrintSetting != null)
                 {
                     duplexPrintSetting.IsEnabled = true;
+                    duplexPrintSetting.Value = (int)Duplex.Off; // TODO: Verify if needs to set to Off since this is not indicated in specifications
+                    _selectedPrinter.PrintSettings.Duplex = (int)Duplex.Off;
 
                     isUpdated = true;
                 }
