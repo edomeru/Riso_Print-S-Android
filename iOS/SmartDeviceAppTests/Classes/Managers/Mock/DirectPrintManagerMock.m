7e780335f89f450e8123ad0335df903b902693dc
Seph <seph@alink-group.co.jp>
2014-05-08 10:11:38 +0900

    #3648 - Add mock for DirectPrintManager - Update test cases in DirectPrintManagerTest

---
+++ iOS/SmartDeviceAppTests/Classes/Managers/Mock/DirectPrintManagerMock.m
@@ -0,0 +1,36 @@
+//
+//  DirectPrintManagerMock.m
+//  SmartDeviceApp
+//
+//  Created by a-LINK Group.
+//  Copyright (c) 2014 RISO KAGAKU CORPORATION. All rights reserved.
+//
+
+#import "DirectPrintManagerMock.h"
+#import "DirectPrintManager.h"
+#include "common.h"
+
+@interface DirectPrintManager()
+
+- (directprint_job *)preparePrintJob;
+- (void)updateSuccess;
+
+@end
+
+@implementation DirectPrintManagerMock
+
+- (void)printDocumentViaLPR
+{
+    id instance = self;
+    [instance preparePrintJob];
+    [instance updateSuccess];
+}
+
+- (void)printDocumentViaRaw
+{
+    id instance = self;
+    [instance preparePrintJob];
+    [instance updateSuccess];
+}
+
+@end
