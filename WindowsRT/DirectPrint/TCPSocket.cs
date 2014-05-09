8bf6b4c7c65751c0fcbc7da035e844dd6108ca08
keith <keith@alink-group.co.jp>
2014-05-09 14:40:04 +0900

    separately check for lpr responses

--- WindowsRT/DirectPrint/TCPSocket.cs
+++ WindowsRT/DirectPrint/TCPSocket.cs
@@ -41,11 +41,12 @@ namespace DirectPrint
             await socket.ConnectAsync(h, port);
         }
 
-        internal async void disconnect()
+        internal void disconnect()
         {
             if (socket != null)
             {
                 socket.Dispose();
+                socket = null;
             }
         }
 
@@ -64,6 +65,8 @@ namespace DirectPrint
                 {
                     dataReceivedHandler(socket.Information.RemoteHostName, responseData);
                 }
+
+                reader.DetachStream();
             }
             catch (Exception exception)
             {
@@ -105,43 +108,6 @@ namespace DirectPrint
                 // detach the stream and close it
                 writer.DetachStream();
                 writer.Dispose();
-
-                // Now try to receive data from server
-                if (waitresponse)
-                try
-                {
-                    DataReader reader = new DataReader(socket.InputStream);
-                    // Set inputstream options so that we don't have to know the data size
-                    await reader.LoadAsync(1);
-                    byte[] responseData = new byte[1];
-                    reader.ReadBytes(responseData);
-
-                    if (dataReceivedHandler != null){
-                        dataReceivedHandler(socket.Information.RemoteHostName,responseData);
-                    }
-                }
-                catch (Exception exception)
-                {
-                    /*
-                    // If this is an unknown status, 
-                    // it means that the error is fatal and retry will likely fail.
-                    if (SocketError.GetStatus(exception.HResult) == SocketErrorStatus.Unknown)
-                    {
-                        throw;
-                    }
-
-                    StatusText.Text = "Receive failed with error: " + exception.Message;
-                    // Could retry, but for this simple example
-                    // just close the socket.
-
-                    closing = true;
-                    clientSocket.Dispose();
-                    clientSocket = null;
-                    connected = false;
-                    */
-                    return;
-                }
-
             }
             
         }    
