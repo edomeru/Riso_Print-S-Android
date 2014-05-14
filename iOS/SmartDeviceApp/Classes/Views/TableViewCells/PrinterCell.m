c5860aafb259f2f0da40b1069e4c8f7b2ae1c83e
Amor Corazon S. Rio <amor.rio@i-bridge.com.ph>
2014-05-14 19:49:59 +0800

    #3648  - Fix separator color when highlighted

--- iOS/SmartDeviceApp/Classes/Views/TableViewCells/PrinterCell.m
+++ iOS/SmartDeviceApp/Classes/Views/TableViewCells/PrinterCell.m
@@ -38,6 +38,7 @@
         self.contentView.backgroundColor = [UIColor purple2ThemeColor];
         [self.printerName setTextColor:[UIColor whiteThemeColor]];
         [self.ipAddress setTextColor:[UIColor whiteThemeColor]];
+        [self.separator setBackgroundColor:[UIColor purple2ThemeColor]];
     }
     else
     {
