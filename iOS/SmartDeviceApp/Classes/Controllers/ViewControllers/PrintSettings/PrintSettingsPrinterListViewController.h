de0651a912913591cedeef509e3782f2f09ba3a2
Amor Corazon S. Rio <amor.rio@i-bridge.com.ph>
2014-05-15 14:31:38 +0800

    #3648  - Fix print settings options and print settings printer list back button state

---
+++ iOS/SmartDeviceApp/Classes/Controllers/ViewControllers/PrintSettings/PrintSettingsPrinterListViewController.h
@@ -0,0 +1,13 @@
+//
+//  PrintSettingsPrinterListViewController.h
+//  RISOSmartPrint
+//
+//  Created by Amor Corazon Rio on 5/15/14.
+//  Copyright (c) 2014 aLink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PrintSettingsPrinterListViewController : UIViewController
+
+@end
