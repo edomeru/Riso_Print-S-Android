63e5a2f6e680ab1ee45e563c75f05ee6853746af
Paulus <paulus@alink-group.co.jp>
2014-05-27 19:56:54 +0900

    #3769 Fixed crash when rotating

--- Android/src/jp/co/riso/smartdeviceapp/view/fragment/PrintPreviewFragment.java
+++ Android/src/jp/co/riso/smartdeviceapp/view/fragment/PrintPreviewFragment.java
@@ -340,6 +340,7 @@ public class PrintPreviewFragment extends BaseFragment implements Callback, PDFF
      *            show print settings button
      */
     public void showPrintSettingsButton(View v, boolean show) {
+        show = true;
         if (v.findViewById(ID_PRINT_BUTTON) != null) {
             if (show) {
                 v.findViewById(ID_PRINT_BUTTON).setVisibility(View.VISIBLE);
