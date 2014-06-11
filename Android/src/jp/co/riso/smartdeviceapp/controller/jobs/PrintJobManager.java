b7e5e5b70036d8fedb2341f8940c79040c32c67e
Princess Boco <princess.boco@i-bridge.com.ph>
2014-06-11 17:06:39 +0800

    #3805 limit 100 jobs per printer, background thread for creation of printJobGroupViews

--- Android/src/jp/co/riso/smartdeviceapp/controller/jobs/PrintJobManager.java
+++ Android/src/jp/co/riso/smartdeviceapp/controller/jobs/PrintJobManager.java
@@ -30,7 +30,7 @@ public class PrintJobManager {
     private static final String C_WHERE_PJB_ID = KeyConstants.KEY_SQL_PRINTJOB_ID + "=?";
     private static final String C_WHERE_PRN_ID = KeyConstants.KEY_SQL_PRINTER_ID + "=?";
     private static final String C_ORDERBY_DATE = KeyConstants.KEY_SQL_PRINTER_ID + " ASC ,"
-            + KeyConstants.KEY_SQL_PRINTJOB_DATE + " DESC";
+            + KeyConstants.KEY_SQL_PRINTJOB_DATE + " DESC," + KeyConstants.KEY_SQL_PRINTJOB_ID + " DESC";
     private static final String C_SEL_PRN_ID = KeyConstants.KEY_SQL_PRINTER_TABLE + "."
             + KeyConstants.KEY_SQL_PRINTER_ID + " IN (SELECT DISTINCT "
             + KeyConstants.KEY_SQL_PRINTER_ID + " FROM " + KeyConstants.KEY_SQL_PRINTJOB_TABLE + ")";
@@ -187,7 +187,34 @@ public class PrintJobManager {
     }
     
     /**
-     * This method inserts the value of the print job to the database.
+     * This method retrieves the id of the oldest print job of a printer 
+     * if the printer contains 100 or more print jobs, else returns -1 
+     * 
+     * @param printerId
+     *            printer ID
+     * @return job id of the oldest print job if the printer contains 100 or more print jobs, else -1 
+     */
+    public int getOldest(int printerId) {
+        int jobId = -1;
+        String[] columns = new String[] {KeyConstants.KEY_SQL_PRINTJOB_ID, "MIN(" +KeyConstants.KEY_SQL_PRINTJOB_DATE+")"};
+        String selection = KeyConstants.KEY_SQL_PRINTER_ID + "=?";
+        String[] selArgs = new String[] {Integer.toString(printerId)};
+        String groupBy = KeyConstants.KEY_SQL_PRINTER_ID;
+        String having = "COUNT("+ KeyConstants.KEY_SQL_PRINTER_ID+") >= 100";
+        String orderBy = KeyConstants.KEY_SQL_PRINTJOB_ID + " ASC";
+        
+        Cursor c = mManager.query(KeyConstants.KEY_SQL_PRINTJOB_TABLE, columns, selection, selArgs, groupBy, having, orderBy);
+        if (c.moveToFirst()) {
+             jobId = c.getInt(c.getColumnIndex(KeyConstants.KEY_SQL_PRINTJOB_ID));
+        }
+        c.close();
+        mManager.close();
+        return jobId;
+    }
+
+    /**
+     * This method inserts the value of the print job to the database and deletes the 
+     * oldest print job of a printer if print jobs >= 100.
      * 
      * @param printJob
      *            the PrintJob object containing the values to be inserted
@@ -195,11 +222,18 @@ public class PrintJobManager {
      */
     private boolean insertPrintJob(PrintJob printJob) {
         ContentValues pjvalues = new ContentValues();
+        int jobId = getOldest(printJob.getPrinterId());
+
+        // if print jobs of a printer is >= 100, delete oldest
+        if (jobId != -1) {
+            deleteWithJobId(jobId);
+        }
+        
         pjvalues.put(KeyConstants.KEY_SQL_PRINTER_ID, printJob.getPrinterId());
         pjvalues.put(KeyConstants.KEY_SQL_PRINTJOB_NAME, printJob.getName());
         pjvalues.put(KeyConstants.KEY_SQL_PRINTJOB_RESULT, printJob.getResult().ordinal());
         pjvalues.put(KeyConstants.KEY_SQL_PRINTJOB_DATE, convertDateToString(printJob.getDate()));
-        return mManager.insert(KeyConstants.KEY_SQL_PRINTJOB_TABLE, null, pjvalues);
+        return mManager.insertOrReplace(KeyConstants.KEY_SQL_PRINTJOB_TABLE, null, pjvalues) != -1;
     }
     
     /**
