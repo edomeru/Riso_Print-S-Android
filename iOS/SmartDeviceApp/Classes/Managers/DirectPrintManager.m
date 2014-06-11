2e1c54e1e2d8f5e1a75e231bd9b8a3897ac852b2
Gino <gino.mempin@i-bridge.com.ph>
2014-06-11 16:26:32 +0800

    #3839 [iOS] move timing of print job history item creation

--- iOS/SmartDeviceApp/Classes/Managers/DirectPrintManager.m
+++ iOS/SmartDeviceApp/Classes/Managers/DirectPrintManager.m
@@ -187,14 +187,15 @@ void printProgressCallback(directprint_job *job, int status, float progress);
 {
     self.isPrinting = NO;
     
+    [PrintJobHistoryHelper createPrintJobFromDocument:self.printDocument withResult:1];
+    
     dispatch_async(dispatch_get_main_queue(), ^{
+        [self.alertView dismiss];
         [AlertHelper displayResult:kAlertResultPrintSuccessful withTitle:kAlertTitleDefault withDetails:nil withDismissHandler:^(CXAlertView *alertView){
             dispatch_async(dispatch_get_main_queue(), ^{
-                [PrintJobHistoryHelper createPrintJobFromDocument:self.printDocument withResult:1];
                 [self.delegate documentDidFinishPrinting:YES];
             });
         }];
-        [self.alertView dismiss];
     });
     
 }
