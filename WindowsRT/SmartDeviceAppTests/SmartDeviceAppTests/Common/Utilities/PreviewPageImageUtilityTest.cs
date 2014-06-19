8c5e87bbba549639d820f94d2b928cae09accf26
lester <lester.valencia@i-bridge.com.ph>
2014-06-19 20:15:36 +0800

    Fixed build errors on UT

---
+++ WindowsRT/SmartDeviceAppTests/SmartDeviceAppTests/Common/Utilities/PreviewPageImageUtilityTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using SmartDeviceApp.Controllers;
+using Windows.Storage;
+using SmartDeviceApp.Common.Utilities;
+using System.Threading.Tasks;
+
+namespace SmartDeviceAppTests.Common.Utilities
+{
+    [TestClass]
+    public class PreviewPageImageUtilityTest
+    {
+
+        [TestMethod]
+        public void Test_GetPagesPerSheet_Off()
+        {
+            int pagesPerSheet = PreviewPageImageUtility.GetPagesPerSheet(0);
+            Assert.AreEqual(1, pagesPerSheet);
+        }
+
+        [TestMethod]
+        public void Test_GetPagesPerSheet_TwoUp()
+        {
+            int pagesPerSheet = PreviewPageImageUtility.GetPagesPerSheet(1);
+            Assert.AreEqual(2, pagesPerSheet);
+        }
+
+        [TestMethod]
+        public void Test_GetPagesPerSheet_FourUp()
+        {
+            int pagesPerSheet = PreviewPageImageUtility.GetPagesPerSheet(2);
+            Assert.AreEqual(4, pagesPerSheet);
+        }
+
+        [TestMethod]
+        public void Test_GetPagesPerSheet_NotExists()
+        {
+            int pagesPerSheet = PreviewPageImageUtility.GetPagesPerSheet(3);
+            Assert.AreEqual(1, pagesPerSheet);
+        }
+
+    }
+}
