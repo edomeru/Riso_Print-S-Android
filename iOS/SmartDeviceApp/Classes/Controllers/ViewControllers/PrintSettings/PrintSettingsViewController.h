d3a70db43d0095827a0a2fa6bf018f43ecd40317
Gino <gino.mempin@i-bridge.com.ph>
2014-06-26 18:50:19 +0800

    #3893 [iOS] update code comments for PrintSettings*Controller classes

--- iOS/SmartDeviceApp/Classes/Controllers/ViewControllers/PrintSettings/PrintSettingsViewController.h
+++ iOS/SmartDeviceApp/Classes/Controllers/ViewControllers/PrintSettings/PrintSettingsViewController.h
@@ -9,8 +9,21 @@
 #import <UIKit/UIKit.h>
 #import "SlidingViewController.h"
 
+/**
+ * Controller for both the "Default Print Settings" and the "Print Settings" screen.
+ * The "Default Print Settings" screen is accessed from the "Printers" screen.\n
+ * The "Print Settings" screen is accessed from the "Print Preview" screen.\n
+ * This serves as the container for the PrintSettingsPrinterViewController.\n
+ */
 @interface PrintSettingsViewController : SlidingViewController
 
+/**
+ * Stores the list index of the printer.
+ * This is only used for the "Default Print Settings" screen, which transitions
+ * from either the PrintersIpadViewController or the PrintersIphoneViewController.\n
+ * In both controllers, the list index indicates which printer was selected.\n\n
+ * If coming from the "Print Preview" screen, this is nil.
+ */
 @property (nonatomic, strong) NSNumber *printerIndex;
 
 @end
