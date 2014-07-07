793ac57a991200c56d7472ab0b4818cfcdc15fa2
Marjun <marcelino.esguerra@i-bridge.com.ph>
2014-07-07 14:52:55 +0800

    [BugFix] [BTS 14459]
    ■ Cause of Bug
    Exception errors are not handled in UDPSocket.cs when sending data.
    
    ■ Correction/Fix Details
    Added try-catch blocks to handle exceptions when sending data.

--- WindowsRT/SNMP/UDPSocket.cs
+++ WindowsRT/SNMP/UDPSocket.cs
@@ -46,7 +46,8 @@ namespace SNMP
 
         internal void close()
         {
-            udpSocket.Dispose();
+            if (udpSocket != null)
+                udpSocket.Dispose();
             //throw new NotImplementedException();
         }
 
@@ -59,8 +60,16 @@ namespace SNMP
         internal async void sendData(byte[] data, string ipAddress, byte port, byte timeout, int p3)
         {
             datacounter = 0;
-
-            HostName host = new HostName(ipAddress);
+            HostName host = null;
+            try
+            {
+                host = new HostName(ipAddress);
+            }
+             catch (Exception e)
+            {
+                if (timeoutHandler != null) timeoutHandler(host, null);
+                return;
+            }
             //HostName host = new HostName("255.255.255.255");
             //HostName host = new HostName("224.0.0.1");            
             //byte[] msg = getSNMPPacket("GET", "public", "1.3.6.1.2.1.1.1.0");//desc 1.3.6.1.2.1.1.1.0 get desc
@@ -79,8 +88,16 @@ namespace SNMP
             if (true)
             {
                 //EndpointPair endpoint = new EndpointPair(null, "", host, p);
+                try
+                {
+                    outputStream = await udpSocket.GetOutputStreamAsync(host, p);
+                }
+                catch (Exception e)
+                {
+                    if (timeoutHandler != null) timeoutHandler(host, null);
+                    return;
+                }
                 
-                outputStream = await udpSocket.GetOutputStreamAsync(host, p);
                 //outputStream = await udpSocket.GetOutputStreamAsync(endpoint);
                 writer = new DataWriter(outputStream);
                 writer.WriteBytes(data);
