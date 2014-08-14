2a12d08d4a603da9da593e6e283b5997bfa22662
lester <lester.valencia@i-bridge.com.ph>
2014-08-14 16:11:30 +0800

    0014896: [WinRT] Truncation of Printer names and IP Address ends in ellipsis - Applied middle text trimming for the printers list in Print Preview Screen

--- WindowsRT/SmartDeviceApp/Converters/MiddleTrimmedTextConverter.cs
+++ WindowsRT/SmartDeviceApp/Converters/MiddleTrimmedTextConverter.cs
@@ -368,6 +368,100 @@ namespace SmartDeviceApp.Converters
     }
 
 
+    public class KeyTextMiddleTrimmedTextConverter : IValueConverter
+    {
+        /// <summary>
+        /// Returns the trimmed text of a Key text (generic)
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="language">The culture to use in the converter.</param>
+        /// <returns>A converted value. If the method returns null, the valid null value is used.</returns>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null || !(value is string) || String.IsNullOrEmpty(value.ToString()) ||
+                parameter == null || !(parameter is double))
+            {
+                return String.Empty;
+            }
+
+            String text = (string)value;
+            Style style = (Style)Application.Current.Resources["STYLE_TextKeyNoTextTrim"];
+
+            double actualWidth = ViewControlUtility.GetTextWidthFromTextBlockWithStyle(text, style);
+            double desiredWidth = (double)parameter;
+            if (actualWidth <= desiredWidth)
+            {
+                return text; // No text trimming
+            }
+
+            text = ViewControlUtility.GetMiddleTrimmedTextFromTextBlockWithStyleAndWidth(text, style, desiredWidth);
+            return text;
+        }
+
+        /// <summary>
+        /// Not implemented.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="language">The culture to use in the converter.</param>
+        /// <returns>A converted value. If the method returns null, the valid null value is used.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class KeySubTextMiddleTrimmedTextConverter : IValueConverter
+    {
+        /// <summary>
+        /// Returns the trimmed text of a Key sub text (generic)
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="language">The culture to use in the converter.</param>
+        /// <returns>A converted value. If the method returns null, the valid null value is used.</returns>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null || !(value is string) || String.IsNullOrEmpty(value.ToString()) ||
+                parameter == null || !(parameter is double))
+            {
+                return String.Empty;
+            }
+
+            String text = (string)value;
+            Style style = (Style)Application.Current.Resources["STYLE_TextKeySubTextNoTextTrim"];
+
+            double actualWidth = ViewControlUtility.GetTextWidthFromTextBlockWithStyle(text, style);
+            double desiredWidth = (double)parameter;
+            if (actualWidth <= desiredWidth)
+            {
+                return text; // No text trimming
+            }
+
+            text = ViewControlUtility.GetMiddleTrimmedTextFromTextBlockWithStyleAndWidth(text, style, desiredWidth);
+            return text;
+        }
+
+        /// <summary>
+        /// Not implemented.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="language">The culture to use in the converter.</param>
+        /// <returns>A converted value. If the method returns null, the valid null value is used.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
     public class ValueTextMiddleTrimmedTextConverter : IValueConverter
     {
         /// <summary>
