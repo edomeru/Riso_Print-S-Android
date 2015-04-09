d0982a757db88ad319d19ae6ffeaa8eba25fac51
keith <keith@alink-group.co.jp>
2015-04-09 16:41:42 +0900

    Merge branch 'bug/17221' into develop

--- Android/src/jp/co/riso/smartdeviceapp/view/fragment/PrintSettingsFragment.java
+++ Android/src/jp/co/riso/smartdeviceapp/view/fragment/PrintSettingsFragment.java
@@ -20,6 +20,7 @@ import jp.co.riso.android.os.pauseablehandler.PauseableHandlerCallback;
 import jp.co.riso.android.util.AppUtils;
 import jp.co.riso.android.util.NetUtils;
 import jp.co.riso.smartprint.R;
+import jp.co.riso.smartdeviceapp.AppConstants;
 import jp.co.riso.smartdeviceapp.SmartDeviceApp;
 import jp.co.riso.smartdeviceapp.common.DirectPrintManager;
 import jp.co.riso.smartdeviceapp.common.DirectPrintManager.DirectPrintCallback;
@@ -32,9 +33,16 @@ import jp.co.riso.smartdeviceapp.model.Printer.PortSetting;
 import jp.co.riso.smartdeviceapp.model.printsettings.PrintSettings;
 import jp.co.riso.smartdeviceapp.view.base.BaseFragment;
 import jp.co.riso.smartdeviceapp.view.printsettings.PrintSettingsView;
+<<<<<<< HEAD
+=======
+import jp.co.riso.smartdeviceapp.view.printsettings.PrintSettingsView.PrintSettingsViewInterface;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+>>>>>>> 6081990... bug fixes
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.Message;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.TextView;
 
@@ -160,7 +168,20 @@ public class PrintSettingsFragment extends BaseFragment implements PrintSettings
      * @param printerId Printer ID
      */
     public void setPrinterId(int printerId) {
-        mPrinterId = printerId;
+        if (mPrinterId != printerId){
+            
+            if (mPrinterId != -1){
+                //reset secure print values
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(SmartDeviceApp.getAppContext());
+                
+                Editor editor = prefs.edit();
+                editor.putBoolean(AppConstants.PREF_KEY_AUTH_SECURE_PRINT, AppConstants.PREF_DEFAULT_AUTH_SECURE_PRINT);
+                editor.putString(AppConstants.PREF_KEY_AUTH_PIN_CODE, AppConstants.PREF_DEFAULT_AUTH_PIN_CODE);
+                editor.commit();
+            }
+        
+            mPrinterId = printerId;
+        }
     }
 
     /**
