82948a11e93aa72ead6d7c2b1f73dd9aaf8db37f
Jay Rinaldi Fatalla <jr@alink-group.co.jp>
2015-04-21 12:50:15 +0900

    [BUG# 17020] Remove the set property of EnabledOpenDocumentCommand, will now completely depend on the reverse of  isProgressRingActive
    (cherry picked from commit bd774b8aba57234ef57afbfe9b242787556f5b0e)

--- WindowsRT/SmartDeviceApp/ViewModels/HomeViewModel.cs
+++ WindowsRT/SmartDeviceApp/ViewModels/HomeViewModel.cs
@@ -39,7 +39,6 @@ namespace SmartDeviceApp.ViewModels
 
         private ICommand _openDocumentCommand;
         private bool _isProgressRingActive;
-        private bool _enabledOpenDocumentCommand;
 
         /// <summary>
         /// HomeViewModel class constructor
@@ -51,7 +50,6 @@ namespace SmartDeviceApp.ViewModels
             _dataService = dataService;
             _navigationService = navigationService;
             _viewControlViewModel = new ViewModelLocator().ViewControlViewModel;
-            EnabledOpenDocumentCommand = true;
             IsProgressRingActive = false;
             Messenger.Default.Register<ViewMode>(this, (viewMode) => EnableMode(viewMode));
         }
@@ -86,6 +84,8 @@ namespace SmartDeviceApp.ViewModels
                 {
                     _isProgressRingActive = value;
                     RaisePropertyChanged("IsProgressRingActive");
+                    // for change in open document command
+                    RaisePropertyChanged("EnabledOpenDocumentCommand");
                 }
             }
         }
@@ -96,23 +96,15 @@ namespace SmartDeviceApp.ViewModels
         /// </summary>
         public bool EnabledOpenDocumentCommand
         {
-            get 
-            { 
-                return _enabledOpenDocumentCommand; 
-            }
-            set
-            {
-                if (_enabledOpenDocumentCommand != value)
-                {
-                    _enabledOpenDocumentCommand = value;
-                    RaisePropertyChanged("EnabledOpenDocumentCommand");
-                }
+            get
+            {   /// this is the reverse value of progress ring active 
+                /// if progress ring is active disable open document command 
+                return !_isProgressRingActive; 
             }
         }
 
         private async void OpenDocumentCommandExecute()
         {
-            EnabledOpenDocumentCommand = false;
             try
             {
                 FileOpenPicker openPicker = new FileOpenPicker();
@@ -144,7 +136,6 @@ namespace SmartDeviceApp.ViewModels
                 LogUtility.LogError(ex);
                 DialogService.Instance.ShowError("IDS_ERR_MSG_OPEN_FAILED", "IDS_APP_NAME", "IDS_LBL_OK", null);
             }
-            EnabledOpenDocumentCommand = true;
         }
 
         private void EnableMode(ViewMode viewMode)
