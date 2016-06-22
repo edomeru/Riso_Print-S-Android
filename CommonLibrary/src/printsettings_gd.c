17acdb0b22527c044405fdb6331b82df670b98d9
Carlo Virtucio <carlo.virtucio@i-bridge.com.ph>
2016-06-22 18:25:56 +0900

    [bug/20201]
    - Updated PJL for GD and FW printers based on latest PS_Driver_Requirements:
        1. Switched order in multiup for 4PAGESLEFTTOPRIGHT with 4PAGESLEFTTOPBOTTOM
        2. Switched order in multiup for 4PAGESRIGHTTOPLEFT with 4PAGESRIGHTTOPBOTTOM
        3. Switched order in finishing side for RIGHTBINDING and TOPBINDING
        4. Added 1STAPLE to staple
        5. Changed RKOWNERNAME to USERNAME
    
    - Now uses 1STAPLE when binding side is Left or Right and 1 staple is selected

--- CommonLibrary/src/printsettings_gd.c
+++ CommonLibrary/src/printsettings_gd.c
@@ -171,11 +171,11 @@ const static char *multiup[] =
 {
     "OFF",
     "2PAGESLEFTRIGHT",
-    "2PAGESRIGHTLEFT",
-    "4PAGESLEFTTOPRIGHT",
+    "2PAGESRIGHTLEFT",    
     "4PAGESLEFTTOPBOTTOM",
-    "4PAGESRIGHTTOPLEFT",
+    "4PAGESLEFTTOPRIGHT",
     "4PAGESRIGHTTOPBOTTOM",
+    "4PAGESRIGHTTOPLEFT",    
 };
 
 const static char *booklet[] =
@@ -195,13 +195,14 @@ const static char *booklet_finishing[] =
 const static char *finishing_side[] =
 {
     "LEFTBINDING",
-    "RIGHTBINDING",
-    "TOPBINDING"
+    "TOPBINDING",
+    "RIGHTBINDING"
 };
 
 const static char *staple[] =
 {
     "OFF",
+    "1STAPLE",
     "1STAPLELEFT",
     "1STAPLERIGHT",
     "2STAPLES"
@@ -293,7 +294,7 @@ const static char *pjl_commands[kPjlCommandCount] =
     "RKSTAPLEMODE",
     "RKPUNCHMODE",
     "RKOUTPUTTRAY",
-    "RKOWNERNAME",
+    "USERNAME",
     "RKSECRETNUM",
     "PASSWORD"
 };
@@ -597,7 +598,7 @@ void add_pjl_gd(char *pjl, char *appName, char *appVersion, setting_value values
                     direction = 1;
                 }
                 int order = 0;
-                if (imposition_order.int_value == 4 || imposition_order.int_value == 5)
+                if (imposition_order.int_value == 2 || imposition_order.int_value == 3)
                 {
                     order = 1;
                 }
@@ -616,25 +617,25 @@ void add_pjl_gd(char *pjl, char *appName, char *appVersion, setting_value values
                 }
                 else if (pagePerSheet == 1 && direction == 0 && order == 0)
                 {
-                    // Nup 4LeftTopToRight
+                    // Nup 4LeftTopToBottom
                     sprintf(pjl_line, PJL_COMMAND_STR, pjl_commands[command], pjl_values[command][3]);
                     strcat(pjl, pjl_line);
                 }
                 else if (pagePerSheet == 1 && direction == 0 && order == 1)
                 {
-                    // Nup 4LeftTopToBottom
+                    // Nup 4LeftTopToRight
                     sprintf(pjl_line, PJL_COMMAND_STR, pjl_commands[command], pjl_values[command][4]);
                     strcat(pjl, pjl_line);
                 }
                 else if (pagePerSheet == 1 && direction == 1 && order == 0)
                 {
-                    // Nup 4RightTopToLeft
+                    // Nup 4RightTopToBottom
                     sprintf(pjl_line, PJL_COMMAND_STR, pjl_commands[command], pjl_values[command][5]);
                     strcat(pjl, pjl_line);
                 }
                 else
                 {
-                    // Nup 4RightTopToBottom
+                    // Nup 4RightTopToLeft
                     sprintf(pjl_line, PJL_COMMAND_STR, pjl_commands[command], pjl_values[command][6]);
                     strcat(pjl, pjl_line);
                 }
@@ -734,33 +735,33 @@ void add_pjl_gd(char *pjl, char *appName, char *appVersion, setting_value values
                     }
                     else if (staple.int_value == 4)
                     {
-                        sprintf(pjl_line, PJL_COMMAND_STR, pjl_commands[command], pjl_values[command][3]);
+                        sprintf(pjl_line, PJL_COMMAND_STR, pjl_commands[command], pjl_values[command][4]);
                     }
                 }
                 else if (finishing_side.int_value == 2)
                 {
                     if (staple.int_value == 3)
                     {
-                        sprintf(pjl_line, PJL_COMMAND_STR, pjl_commands[command], pjl_values[command][2]);
+                        sprintf(pjl_line, PJL_COMMAND_STR, pjl_commands[command], pjl_values[command][1]);
                     }
                     else if (staple.int_value == 4)
                     {
-                        sprintf(pjl_line, PJL_COMMAND_STR, pjl_commands[command], pjl_values[command][3]);
+                        sprintf(pjl_line, PJL_COMMAND_STR, pjl_commands[command], pjl_values[command][4]);
                     }
                 }
                 else if (finishing_side.int_value == 1)
                 {
                     if (staple.int_value == 1)
                     {
-                        sprintf(pjl_line, PJL_COMMAND_STR, pjl_commands[command], pjl_values[command][1]);
+                        sprintf(pjl_line, PJL_COMMAND_STR, pjl_commands[command], pjl_values[command][2]);
                     }
                     else if (staple.int_value == 2)
                     {
-                        sprintf(pjl_line, PJL_COMMAND_STR, pjl_commands[command], pjl_values[command][2]);
+                        sprintf(pjl_line, PJL_COMMAND_STR, pjl_commands[command], pjl_values[command][3]);
                     }
                     else if (staple.int_value == 4)
                     {
-                        sprintf(pjl_line, PJL_COMMAND_STR, pjl_commands[command], pjl_values[command][3]);
+                        sprintf(pjl_line, PJL_COMMAND_STR, pjl_commands[command], pjl_values[command][4]);
                     }
                 }
             }
