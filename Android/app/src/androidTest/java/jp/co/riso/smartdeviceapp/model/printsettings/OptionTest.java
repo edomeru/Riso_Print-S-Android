664ce5dfbf0a5c0ca81f32ebb8f6362cb3ca55f6
Carlo Virtucio <carlo.virtucio@i-bridge.com.ph>
2016-06-22 12:12:12 +0900

    [developFromRISO-201605] Minor UT Fixes

--- Android/app/src/androidTest/java/jp/co/riso/smartdeviceapp/model/printsettings/OptionTest.java
+++ Android/app/src/androidTest/java/jp/co/riso/smartdeviceapp/model/printsettings/OptionTest.java
@@ -66,7 +66,7 @@ public class OptionTest extends AndroidTestCase {
     }
 
     public void testPreConditions() {
-        assertEquals(204, mOptionList.size());
+        assertEquals(202, mOptionList.size());
     }
 
     public void testGetTextContent() {
