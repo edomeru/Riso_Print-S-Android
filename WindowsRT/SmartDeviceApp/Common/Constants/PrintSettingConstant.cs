d1d04270c1433888d8860d7f05d33400b1bad47e
amor <amor.rio@i-bridge.com.ph>
2016-07-12 00:52:20 -0700

    [RQM-1511-004-winrt] Add paper sizes for preview for Legal13, 8K and 16K

--- WindowsRT/SmartDeviceApp/Common/Constants/PrintSettingConstant.cs
+++ WindowsRT/SmartDeviceApp/Common/Constants/PrintSettingConstant.cs
@@ -66,6 +66,9 @@ namespace SmartDeviceApp.Common.Constants
         public static Size PAPER_SIZE_LEGAL      = new Size(216.0, 356.0);
         public static Size PAPER_SIZE_LETTER     = new Size(216.0, 280.0);
         public static Size PAPER_SIZE_STATEMENT  = new Size(140.0, 216.0);
+        public static Size PAPER_SIZE_LEGAL13    = new Size(216.0, 333.0);
+        public static Size PAPER_SIZE_8K         = new Size(270.0, 390.0);
+        public static Size PAPER_SIZE_16K        = new Size(195.0, 270.0);
 
         // Margin; in inches
         public const double MARGIN_IMPOSITION_EDGE          = 0;
