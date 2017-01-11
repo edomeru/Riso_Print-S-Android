2c4291bb16b6b384d18f2c0817fc5d11080cc21c
Princess Boco <princess.boco@i-bridge.com.ph>
2017-01-11 11:04:40 +0800

    [Android] Base code from RISO-PRINT-S_src_android_2.0.4.6.zip 20161228 (copied Android and CommonLibrary source codes)

--- Android/app/src/main/java/jp/co/riso/smartdeviceapp/model/printsettings/PrintSettings.java
+++ Android/app/src/main/java/jp/co/riso/smartdeviceapp/model/printsettings/PrintSettings.java
@@ -230,8 +230,12 @@ public class PrintSettings {
                 }
             }
 
-            if(key.equals(TAG_PUNCH) && value == 3) {
+            if(mSettingMapKey == AppConstants.PRINTER_MODEL_IS && key.equals(TAG_PUNCH) && value == 3) {
                 value = 2;
+            }
+
+            if(key.equals(TAG_SORT) && mSettingMapKey == AppConstants.PRINTER_MODEL_IS) {
+                value ^= 1;
             }
 
             strBuf.append(String.format(Locale.getDefault(), KEY_VAL_FORMAT, key, value));
