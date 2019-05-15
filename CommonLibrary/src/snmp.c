366f6eb6a282dc6ae312b56c724a8e6679f26341
Pabs <pabs@alink-group.co.jp>
2019-05-15 15:59:05 +0800

    [feature/V4.0.0/LIO-support-android] Add LIO support in common.h and snmp.c

--- CommonLibrary/src/snmp.c
+++ CommonLibrary/src/snmp.c
@@ -31,6 +31,7 @@
 #define SNMPV3_PASS "risosnmp"
 
 #define RAG_PRINTER_TYPE "RAG"
+#define LIO_PRINTER_TYPE "LIO"
 
 #define DETECT_ALL_DEVICES 0  // 0 for RISO only
 
@@ -661,6 +662,12 @@ int snmp_device_get_series(snmp_device *device)
     {
         // RAG Series
         return kPrinterSeriesRAG;
+    }
+
+    if (strstr(device->device_info[MIB_DEV_DESCR], LIO_PRINTER_TYPE) != NULL)
+    {
+        // LIO Series
+        return kPrinterSeriesLIO;
     }
 
     return kPrinterSeriesGD;
