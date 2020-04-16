23f7c5310a8d8fba497ed2eab6ab821de02bbbd6
mau <mau.clores@alink-group.co.jp>
2020-04-16 14:18:17 +0900

    Added HomeFragment class

--- Android/app/src/main/java/jp/co/riso/android/util/FileUtils.java
+++ Android/app/src/main/java/jp/co/riso/android/util/FileUtils.java
@@ -8,10 +8,12 @@
 
 package jp.co.riso.android.util;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.OpenableColumns;
+import android.webkit.MimeTypeMap;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -79,6 +81,29 @@ public class FileUtils {
         
         src.delete();
         
+    }
+
+    /**
+     * @brief Determines the mime type of a file uri
+     *
+     * @param context Application context
+     * @param uri File uri
+     * @return Mime type of file
+     */
+    public static String getMimeType(Context context, Uri uri) {
+        String mimeType = "";
+
+        if (context != null && uri != null && uri.getScheme() != null) {
+            if (uri.getScheme().equals(ContentResolver.SCHEME_CONTENT)) {
+                ContentResolver cr = context.getContentResolver();
+                mimeType = cr.getType(uri);
+            } else {
+                String fileExtension = MimeTypeMap.getFileExtensionFromUrl(uri.toString());
+                mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(fileExtension.toLowerCase());
+            }
+        }
+
+        return mimeType == null ? "" : mimeType;
     }
 
     /**
