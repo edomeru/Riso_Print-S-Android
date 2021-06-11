10294f24292068d92a965cc8bde38647ba16bebd
Lori <lori@alink-group.co.jp>
2021-06-11 20:18:49 +0800

    [bug/V5.3.0/Android/FireOS7_896_preview-pixelated2] Add comments to all third-party modifications

--- Android/app/src/main/java/fi/harism/curl/CurlPage.java
+++ Android/app/src/main/java/fi/harism/curl/CurlPage.java
@@ -141,12 +141,16 @@ public class CurlPage {
 	 * Recycles and frees underlying Bitmaps.
 	 */
 	public void recycle() {
+		// aLINK edit: check if the bitmap has already been recycled
 		if (mTextureFront != null && !mTextureFront.isRecycled()) {
+		// end
 			mTextureFront.recycle();
 		}
 		mTextureFront = Bitmap.createBitmap(1, 1, Bitmap.Config.RGB_565);
 		mTextureFront.eraseColor(mColorFront);
+		// aLINK edit: check if the bitmap has already been recycled
 		if (mTextureBack != null && !mTextureBack.isRecycled()) {
+		// end
 			mTextureBack.recycle();
 		}
 		mTextureBack = Bitmap.createBitmap(1, 1, Bitmap.Config.RGB_565);
