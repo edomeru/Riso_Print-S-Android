bd2c81672c897f02855ab6517f142b3fe99c2923
Dexter Andino <dex@alink-group.co.jp>
2021-06-17 12:38:33 +0800

    [bug/V5.3.0/Android/ChromeOS_942_print_job_history_rotate] removed import log

--- Android/app/src/main/java/jp/co/riso/smartdeviceapp/view/jobs/PrintJobsView.java
+++ Android/app/src/main/java/jp/co/riso/smartdeviceapp/view/jobs/PrintJobsView.java
@@ -24,7 +24,6 @@ import android.content.Context;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.TranslateAnimation;
