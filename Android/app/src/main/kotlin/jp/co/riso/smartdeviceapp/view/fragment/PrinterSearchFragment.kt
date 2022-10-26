6cd36c04922b665397d897bcb256e1ee5eb35d11
Marco Jedric Nicolas <jedric@alink-group.co.jp>
2022-10-26 12:54:32 +0900

    Apply suggestion to Android/app/src/main/kotlin/jp/co/riso/smartdeviceapp/view/fragment/PrinterSearchFragment.kt

--- Android/app/src/main/kotlin/jp/co/riso/smartdeviceapp/view/fragment/PrinterSearchFragment.kt
+++ Android/app/src/main/kotlin/jp/co/riso/smartdeviceapp/view/fragment/PrinterSearchFragment.kt
@@ -64,7 +64,7 @@ class PrinterSearchFragment : BaseFragment(), PullToRefreshListView.OnRefreshLis
     }
 
     private inline fun <reified T : Parcelable> Bundle.parcelableArrayList(key: String): ArrayList<T>? = when {
-        SDK_INT >= 33 -> getParcelableArrayList(key, T::class.java)
+        SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelableArrayList(key, T::class.java)
         else -> @Suppress("DEPRECATION") getParcelableArrayList(key)
     }
 
