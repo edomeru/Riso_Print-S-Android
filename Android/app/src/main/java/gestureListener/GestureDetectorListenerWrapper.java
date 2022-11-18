c6353b3a238cfae7869f76f1956766f6aa2f0e62
Marco Jedric V. Nicolas <jedric@alink-group.co.jp>
2022-11-18 15:28:39 +0900

    Add new wrapper java file for OnGestureListener

---
+++ Android/app/src/main/java/gestureListener/GestureDetectorListenerWrapper.java
@@ -0,0 +1,44 @@
+package gestureListener;
+
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+import androidx.annotation.NonNull;
+
+/* Android 13 New OS Support - Create Java wrapper for GestureDetector.OnGestureListener
+   to avoid crashing in API 33 + Kotlin
+ */
+
+public interface GestureDetectorListenerWrapper extends GestureDetector.OnGestureListener {
+
+    @Override
+    default boolean onDown(@NonNull MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    default void onShowPress(@NonNull MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    default boolean onSingleTapUp(@NonNull MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    default boolean onScroll(@NonNull MotionEvent motionEvent, @NonNull MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+
+    @Override
+    default void onLongPress(@NonNull MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    default boolean onFling(@NonNull MotionEvent motionEvent, @NonNull MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+}
+
