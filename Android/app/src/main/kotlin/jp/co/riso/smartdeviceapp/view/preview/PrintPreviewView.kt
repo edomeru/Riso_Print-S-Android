560aa9fd6782fec9691fd1a37c48b417df525410
Marco Jedric V. Nicolas <jedric@alink-group.co.jp>
2022-12-08 20:31:04 +0900

    Fix for #1256 for Android side

--- Android/app/src/main/kotlin/jp/co/riso/smartdeviceapp/view/preview/PrintPreviewView.kt
+++ Android/app/src/main/kotlin/jp/co/riso/smartdeviceapp/view/preview/PrintPreviewView.kt
@@ -1507,7 +1507,7 @@ class PrintPreviewView @JvmOverloads constructor(
         const val BASE_ZOOM_LEVEL = 1.0f
 
         /// Maximum zoom level of the preview
-        const val MAX_ZOOM_LEVEL = 4.0f
+        const val MAX_ZOOM_LEVEL = 10.0f
         private const val INVALID_IDX = -1
         private const val SLEEP_DELAY = 100
         private const val SMALL_BMP_SIZE = 64
