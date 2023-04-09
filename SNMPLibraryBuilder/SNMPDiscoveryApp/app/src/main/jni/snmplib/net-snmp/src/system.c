355d91459e0a990eaa1a48c04131359233d51324
Marco Jedric V. Nicolas <jedric@alink-group.co.jp>
2023-04-09 19:28:12 +0900

    Can genearate netsnmp.so for arm and x86

--- SNMPLibraryBuilder/SNMPDiscoveryApp/app/src/main/jni/snmplib/net-snmp/src/system.c
+++ SNMPLibraryBuilder/SNMPDiscoveryApp/app/src/main/jni/snmplib/net-snmp/src/system.c
@@ -28,7 +28,7 @@ SOFTWARE.
 ******************************************************************/
 /*
  * Portions of this file are copyrighted by:
- * Copyright © 2003 Sun Microsystems, Inc. All rights reserved.
+ * Copyright ï¿½ 2003 Sun Microsystems, Inc. All rights reserved.
  * Use is subject to license terms specified in the COPYING file
  * distributed with the Net-SNMP package.
  */
@@ -1260,10 +1260,10 @@ netsnmp_mktemp(void)
     strlcpy(name, get_temp_file_pattern(), sizeof(name));
 #ifdef HAVE_MKSTEMP
     {
-        mode_t oldmask = umask(~(S_IRUSR | S_IWUSR));
-        netsnmp_assert(oldmask != (mode_t)(-1));
-        fd = mkstemp(name);
-        umask(oldmask);
+//        mode_t oldmask = umask(~(S_IRUSR | S_IWUSR));
+//        netsnmp_assert(oldmask != (mode_t)(-1));
+//        fd = mkstemp(name);
+//        umask(oldmask);
     }
 #else
     if (mktemp(name)) {
