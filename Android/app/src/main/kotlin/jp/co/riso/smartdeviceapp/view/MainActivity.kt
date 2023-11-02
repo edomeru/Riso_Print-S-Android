1f52fc9200890724ee78229a27519bee4564b97d
Marco Jedric V. Nicolas <jedric@alink-group.co.jp>
2023-11-02 12:21:08 +0900

    Fix build error, Update gradle plugin version

--- Android/app/src/main/kotlin/jp/co/riso/smartdeviceapp/view/MainActivity.kt
+++ Android/app/src/main/kotlin/jp/co/riso/smartdeviceapp/view/MainActivity.kt
@@ -74,7 +74,7 @@ class MainActivity : BaseActivity(), PauseableHandlerCallback {
             *  Permission will only be applied for images
             * */
             val contentType = FileUtils.getMimeType(this, intent.data)
-            if (contentType != AppConstants.DOC_TYPES[0] && contentType != AppConstants.DOC_TYPES[1]) {git 
+            if (contentType != AppConstants.DOC_TYPES[0] && contentType != AppConstants.DOC_TYPES[1]) {
                 // WRITE_EXTERNAL_STORAGE for Android 12 and older versions
                 var permissionType = Manifest.permission.WRITE_EXTERNAL_STORAGE
 
